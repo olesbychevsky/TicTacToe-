fa0f6c08db6e0c021435ad70fe9da477eff77db7
Oles <olesbychevskyi@gmail.com>
2021-12-17 12:00:24 +0200

    init

---
+++ src/com/company/TicTacToe.java
@@ -0,0 +1,40 @@
+package com.company;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+public class TicTacToe implements ActionListener {
+
+    Random random = new Random();
+    JFrame frame = new JFrame();
+    JPanel title_panel = new JPanel();
+    JPanel button_panel = new JPanel();
+    JLabel textField = new JLabel();
+
+    TicTacToe() {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    public void firstTurn() {
+
+    }
+
+    public void check() {
+
+    }
+
+    public void xWins(int a, int b, int c) {
+
+    }
+
+    public void oWins(int a, int b, int c) {
+
+    }
+}
