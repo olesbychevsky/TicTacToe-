dccfa9eebb70451eba91f6427f4eb57061f98648
Oles <olesbychevskyi@gmail.com>
2021-12-17 16:17:28 +0200

    actionPerformed() added

--- src/com/company/TicTacToe.java
+++ src/com/company/TicTacToe.java
@@ -18,22 +18,22 @@ public class TicTacToe implements ActionListener {
     TicTacToe() {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 800);
-        frame.getContentPane().setBackground(new Color(50,50,50));
+        frame.getContentPane().setBackground(new Color(50, 50, 50));
         frame.setLayout(new BorderLayout());
         frame.setVisible(true);
 
-        textField.setBackground(new Color(25,25,25));
-        textField.setForeground(new Color(25,255,0));
-        textField.setFont(new Font("Ink Free", Font.BOLD,75));
+        textField.setBackground(new Color(25, 25, 25));
+        textField.setForeground(new Color(25, 255, 0));
+        textField.setFont(new Font("Ink Free", Font.BOLD, 75));
         textField.setHorizontalAlignment(JLabel.CENTER);
         textField.setText("Tic-Tac-Toe");
         textField.setOpaque(true);
 
         title_panel.setLayout(new BorderLayout());
-        title_panel.setBounds(0,0,800,100);
+        title_panel.setBounds(0, 0, 800, 100);
 
-        button_panel.setLayout(new GridLayout(3,3));
-        button_panel.setBackground(new Color(150,150,150));
+        button_panel.setLayout(new GridLayout(3, 3));
+        button_panel.setBackground(new Color(150, 150, 150));
 
         for (int i = 0; i < 9; i++) {
             buttons[i] = new JButton();
@@ -53,7 +53,27 @@ public class TicTacToe implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        for (int i = 0; i < 9; i++) {
+            if (e.getSource() == buttons[i]) {
+                if (player1_turn) {
+                    if (buttons[i].getText() == "") {
+                        buttons[i].setForeground(new Color(255, 0, 0));
+                        buttons[i].setText("X");
+                        player1_turn = false;
+                        textField.setText("0 turn");
+                        check();
+                    }
+                } else {
+                    if (buttons[i].getText() == "") {
+                        buttons[i].setForeground(new Color(0, 0, 255));
+                        buttons[i].setText("0");
+                        player1_turn = true;
+                        textField.setText("X turn");
+                        check();
+                    }
+                }
+            }
+        }
     }
 
     public void firstTurn() {
@@ -62,7 +82,7 @@ public class TicTacToe implements ActionListener {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        if(random.nextInt(2)==0) {
+        if (random.nextInt(2) == 0) {
             player1_turn = true;
             textField.setText("X turn");
         } else {
@@ -83,3 +103,4 @@ public class TicTacToe implements ActionListener {
 
     }
 }
+
